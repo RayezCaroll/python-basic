f44aca12cf890636d77495dcd0b4e82abbd8accb
andrew <foryoma.caroll@gmail.com>
2025-06-16 12:49:12 +0700

    25

---
+++ 26-read.py
@@ -0,0 +1,23 @@
+# file=open('./text.txt')
+
+# for line in file:
+#     print(line)
+
+# file.seek(0) # Move cursor to top of the file
+
+# lineLists=file.readlines() # 
+# print(lineLists)
+
+# file.seek(50) # Move cursor to top of the file
+
+# paragraph=file.read(100)
+# print(paragraph)
+
+# file.close() # To close opened file.
+
+with open('./text.txt') as file: # Do not need to close the file again.
+    for line in file:
+        print(line)
+
+print("Other Work")
+
